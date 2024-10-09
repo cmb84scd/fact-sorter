c0f8634951888afe329efe1948c3f55b7aa64051
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2024-10-09 12:29:59 +0100

    Add env and region to stack

--- app.py
+++ app.py
@@ -2,9 +2,16 @@
 """Create the eventbus-learning stack."""
 
 import aws_cdk as cdk
+from decouple import config
 from eventbus_learning.infrastructure.stack import EventbusLearningStack
 
 app = cdk.App()
-EventbusLearningStack(app, "eventbus-learning")
+EventbusLearningStack(
+    app,
+    "eventbus-learning",
+    env=cdk.Environment(
+        account=config("AWS_ACCOUNT_ID"), region=config("AWS_REGION")
+    ),
+)
 
 app.synth()
