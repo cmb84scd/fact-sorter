1f61d93d8617c5ac6538d8356fdd3fa066a2eae8
Catriona Bennett <catrionamairi@blueyonder.co.uk>
2024-10-14 15:56:33 +0100

    Add iam role factory and amend lambda factory

--- tests/unit_tests/factories/__init__.py
+++ tests/unit_tests/factories/__init__.py
@@ -1,3 +1,3 @@
-from .stack_resources import lambda_properties
+from .stack_resources import iam_role_properties, lambda_properties
 
-__all__ = ["lambda_properties"]
+__all__ = ["iam_role_properties", "lambda_properties"]
--- tests/unit_tests/infrastructure/test_stack.py
+++ tests/unit_tests/infrastructure/test_stack.py
@@ -1,8 +1,8 @@
 from aws_cdk import App
-from aws_cdk.assertions import Capture, Match, Template
+from aws_cdk.assertions import Capture, Template
 from eventbus_learning.infrastructure.stack import EventbusLearningStack
 
-from ..factories import lambda_properties
+from ..factories import iam_role_properties, lambda_properties
 
 app = App()
 stack = EventbusLearningStack(app, "eventbus-learning")
@@ -16,39 +16,16 @@ class TestEventBusLearningStack:
     def test_get_fact_lambda_has_correct_properties(self):
         dependency_capture = Capture()
         template.has_resource_properties(
-            "AWS::Lambda::Function", lambda_properties(dependency_capture)
+            "AWS::Lambda::Function",
+            lambda_properties("get_fact.handler", dependency_capture),
         )
 
         assert "GetFactFunctionServiceRole" in dependency_capture.as_string()
 
-    def test_lambda_has_correct_iam_role(self):
+    def test_get_fact_lambda_has_correct_iam_role(self):
         role_capture = Capture()
         template.has_resource_properties(
-            "AWS::IAM::Role",
-            {
-                "AssumeRolePolicyDocument": {
-                    "Statement": [
-                        {
-                            "Action": "sts:AssumeRole",
-                            "Effect": "Allow",
-                            "Principal": {"Service": "lambda.amazonaws.com"},
-                        }
-                    ],
-                },
-                "ManagedPolicyArns": [
-                    {
-                        "Fn::Join": Match.array_with(
-                            [
-                                [
-                                    "arn:",
-                                    {"Ref": "AWS::Partition"},
-                                    role_capture,
-                                ]
-                            ]
-                        )
-                    }
-                ],
-            },
+            "AWS::IAM::Role", iam_role_properties(role_capture)
         )
 
         assert "AWSLambdaBasicExecutionRole" in role_capture.as_string()
